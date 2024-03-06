af51bcf125bc4b170d3e583a27bc9916507bdd81
Luiz Gustavo de Oliveira Cunha <luiz.gustavogz02@gmail.com>
2024-03-06 11:57:54 -0300

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -1,5 +1,30 @@
+CREATE DATABASE RestauranteDB
+GO
+
 USE RestauranteDB
 GO
 
-SELECT * FROM Pedido
-SELECT * FROM ItensPedido
+CREATE TABLE Pedido (
+    PedidoId INT PRIMARY KEY IDENTITY,
+    NomeSolicitante VARCHAR(100),
+    Mesa INT
+)
+GO
+
+CREATE TABLE ItensPedido (
+    ItemId INT PRIMARY KEY IDENTITY,
+    PedidoId INT,
+    Prato VARCHAR(100),
+    Bebida VARCHAR(100),
+    Quantidade INT,
+    FOREIGN KEY (PedidoId) REFERENCES Pedido(PedidoId)
+)
+GO
+
+
+ SELECT * FROM Pedido
+ SELECT * FROM ItensPedido
+
+-- TRUNCATE TABLE Pedido
+-- TRUNCATE TABLE ItensPedido
+
